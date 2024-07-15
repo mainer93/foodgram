674046c7b035b60310439388c6184d64656384bd
Александр Заваленов <mainer93@yandex.ru>
2024-07-15 23:11:02 +0400

    5ver

--- backend/recipes/management/commands/import_csv.py
+++ backend/recipes/management/commands/import_csv.py
@@ -1,5 +1,7 @@
 import csv
+import os
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
@@ -8,8 +10,14 @@ class Command(BaseCommand):
     help = 'Импортирует данные из CSV файла в модель Ingredient'
 
     def handle(self, *args, **kwargs):
-        csv_file = 'data/ingredients.csv'
-        with open(csv_file, 'r', encoding='utf-8-sig') as file:
+        csv_file_path = os.path.join(settings.BASE_DIR, 'data',
+                                     'ingredients.csv')
+        if not os.path.exists(csv_file_path):
+            self.stdout.write(self.style.ERROR(
+                'Файл не найден: ' + csv_file_path))
+            return
+
+        with open(csv_file_path, 'r', encoding='utf-8-sig') as file:
             reader = csv.reader(file)
             for row in reader:
                 name, measurement_unit = row
