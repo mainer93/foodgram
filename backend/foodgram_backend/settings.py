9db7a47775a980dba2a1e3b9fce1c0f94609f7f6
Александр Заваленов <mainer93@yandex.ru>
2024-07-25 17:22:02 +0400

    gfgdfg

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n
+# https://docs.djangoproject.com/en/3.2/topics/i18
 
 LANGUAGE_CODE = 'en-us'
 
